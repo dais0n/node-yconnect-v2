6f9d5b636c91065c2a3ecd1ac369f6645b00b9e7
dais0n <t.omura8383@gmail.com>
2018-01-16 01:58:55 +0900

    add eslint

--- src/idtoken.js
+++ src/idtoken.js
@@ -1,88 +1,88 @@
-const jwt = require('jsonwebtoken');
+const jwt = require( "jsonwebtoken" );
 
-const issuer       = 'https://auth.login.yahoo.co.jp/yconnect/v2';
-const requiredKeys = ['iss', 'sub', 'aud', 'exp', 'iat', 'amr', 'nonce', 'at_hash'];
+const issuer = "https://auth.login.yahoo.co.jp/yconnect/v2";
+const requiredKeys = [ "iss", "sub", "aud", "exp", "iat", "amr", "nonce", "at_hash" ];
 
 class IdToken {
-  constructor(token) {
-    this.token= token;
-    this.decodedToken = jwt.decode(token, {complete: true});
-  }
-
-  getKid() {
-    return this.decodedToken.header.kid;
-  }
-
-  checkFormat() {
-    for (var key of requiredKeys) {
-      if (!(key in this.decodedToken.payload)) {
-        return false;
-      }
+    constructor( token ) {
+        this.token = token;
+        this.decodedToken = jwt.decode( token, { "complete": true } );
     }
-    return true;
-  }
 
-  checkExpTime(now) {
-    if (now > this.decodedToken.payload['exp']) {
-      return false;
+    getKid() {
+        return this.decodedToken.header.kid;
     }
-    return true;
-  }
 
-  checkIatTime(now) {
+    checkFormat() {
+        for ( let key of requiredKeys ) {
+            if ( !( key in this.decodedToken.payload ) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkExpTime( now ) {
+        if ( now > this.decodedToken.payload.exp ) {
+            return false;
+        }
+        return true;
+    }
+
+    checkIatTime( now ) {
     // check iat within 10 minuts from now
-    let acceptable_range = 600;
-    let timeDiff = now - this.decodedToken.payload['iat'];
-    if (timeDiff > acceptable_range) {
-      return false;
-    }
-    return true;
-  }
+        let acceptableRange = 600;
+        let timeDiff = now - this.decodedToken.payload.iat;
 
-  checkPayload(clientId, nonce, now) {
+        if ( timeDiff > acceptableRange ) {
+            return false;
+        }
+        return true;
+    }
+
+    checkPayload( clientId, nonce, now ) {
     // check format
-    if (!this.checkFormat()) {
-      return false;
+        if ( !this.checkFormat() ) {
+            return false;
+        }
+
+        // check issuer
+        if ( issuer !== this.decodedToken.payload.iss ) {
+            return false;
+        }
+
+        // check nonce
+        if ( nonce !== this.decodedToken.payload.nonce ) {
+            return false;
+        }
+
+        // check client_id
+        if ( clientId !== this.decodedToken.payload.aud ) {
+            return false;
+        }
+
+        // Is corrent time less than exp ?
+        if ( !this.checkExpTime( now ) ) {
+            return false;
+        }
+
+        // check iat within 10 minuts from now
+        if ( !this.checkIatTime( now ) ) {
+            return false;
+        }
+
+        return true;
     }
 
-    // check issuer
-    if (issuer != this.decodedToken.payload['iss']) {
-      return false;
+    verifySignature( pubKey ) {
+        return jwt.verify( this.token, pubKey, { "algorithms": [ "RS256" ] }, ( err, payload ) => {
+            // if token alg !== RS256,  err !== invalid signature
+            if ( err !== null ) {
+                return false;
+            }
+            return true;
+        } );
     }
-
-    // check nonce
-    if (nonce != this.decodedToken.payload['nonce']) {
-      return false;
-    }
-
-    // check client_id
-    if (clientId != this.decodedToken.payload['aud']) {
-      return false;
-    }
-
-    // Is corrent time less than exp ?
-    if (!this.checkExpTime(now)) {
-      return false;
-    }
-
-    // check iat within 10 minuts from now
-    if (!this.checkIatTime(now)) {
-      console.log('invalid iat');
-      return false;
-    }
-
-    return true;
-  }
-
-  verifySignature(pubKey) {
-    return jwt.verify(this.token, pubKey, { algorithms: ['RS256'] }, function (err, payload) {
-      // if token alg != RS256,  err == invalid signature
-      if (err != null) {
-        return false;
-      }
-      return true;
-    });
-  }
 }
 
 module.exports = IdToken;
--- src/yconnect.js
+++ src/yconnect.js
@@ -1,102 +1,102 @@
-const request = require('request-promise');
-const IdToken = require('./idtoken');
+const request = require( "request-promise" );
+const IdToken = require( "./idtoken" );
 
-const tokenUrl    = 'https://auth.login.yahoo.co.jp/yconnect/v2/token';
-const pubKeyUrl   = 'https://auth.login.yahoo.co.jp/yconnect/v2/public-keys';
-const userInfoUrl = 'https://userinfo.yahooapis.jp/yconnect/v2/attribute';
+const tokenUrl = "https://auth.login.yahoo.co.jp/yconnect/v2/token";
+const pubKeyUrl = "https://auth.login.yahoo.co.jp/yconnect/v2/public-keys";
+const userInfoUrl = "https://userinfo.yahooapis.jp/yconnect/v2/attribute";
 
 class yconnect {
-  constructor(clientId, clientSec, redirectUri) {
-    this.clientId    = clientId;
-    this.clientSec   = clientSec;
-    this.redirectUri = redirectUri
-  }
+    constructor( clientId, clientSec, redirectUri ) {
+        this.clientId = clientId;
+        this.clientSec = clientSec;
+        this.redirectUri = redirectUri;
+    }
 
-  setIdToken(idToken) {
-    this.idToken = new IdToken(idToken);
-  }
+    setIdToken( idToken ) {
+        this.idToken = new IdToken( idToken );
+    }
 
-  authorization(code, nonce) {
-    return new Promise((resolve, reject) => {
-      this.tokenRequest(code)
-        .then((response) => {
-          this.setIdToken(response['id_token']);
-          let accessToken = response['access_token']
-          // get now
-          let date = new Date();
-          let tmpDate = date.getTime();
-          // format
-          let now = Math.floor( tmpDate / 1000 );
+    authorization( code, nonce ) {
+        return new Promise( ( resolve, reject ) => {
+            this.tokenRequest( code )
+                .then( ( tokenResponse ) => {
+                    this.setIdToken( tokenResponse.id_token );
+                    let accessToken = tokenResponse.access_token;
+                    // get now
+                    let date = new Date();
+                    let tmpDate = date.getTime();
+                    // format
+                    let now = Math.floor( tmpDate / 1000 );
 
-          if (!this.idToken.checkPayload(this.clientId, nonce, now)) {
-            reject('check payload failed');
-          }
-          this.pubKeyRequest(this.idToken.getKid())
-            .then((response) => {
-              if (!this.idToken.verifySignature(response[this.idToken.getKid()])) {
-                reject('check signature failed');
-              }
-              resolve(accessToken);
-            })
-            .catch((error) => {
-              reject(error);
-            });
-        })
-        .catch((error) => {
-          reject(error)
-        });
-    });
-  }
+                    if ( !this.idToken.checkPayload( this.clientId, nonce, now ) ) {
+                        reject( "check payload failed" );
+                    }
+                    this.pubKeyRequest( this.idToken.getKid() )
+                        .then( ( keyResponse ) => {
+                            if ( !this.idToken.verifySignature( keyResponse[ this.idToken.getKid() ] ) ) {
+                                reject( "check signature failed" );
+                            }
+                            resolve( accessToken );
+                        } )
+                        .catch( ( error ) => {
+                            reject( error );
+                        } );
+                } )
+                .catch( ( error ) => {
+                    reject( error );
+                } );
+        } );
+    }
 
-  tokenRequest(code) {
-    return new Promise((resolve, reject) => {
-      request({
-        uri: tokenUrl,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        form: {
-          code: code,
-          client_id: this.clientId,
-          client_secret: this.clientSec,
-          redirect_uri: this.redirectUri,
-          grant_type: 'authorization_code'
-        },
-        json: true
-      })
-      .then((response) => {
-        resolve(response);
-      })
-      .catch((error) => {
-        reject('token request error');
-      })
-    });
-  }
+    tokenRequest( code ) {
+        return new Promise( ( resolve, reject ) => {
+            request( {
+                "uri": tokenUrl,
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                "form": {
+                    "code": code,
+                    "client_id": this.clientId,
+                    "client_secret": this.clientSec,
+                    "redirect_uri": this.redirectUri,
+                    "grant_type": "authorization_code"
+                },
+                "json": true
+            } )
+                .then( ( response ) => {
+                    resolve( response );
+                } )
+                .catch( ( error ) => {
+                    reject( error );
+                } );
+        } );
+    }
 
-  pubKeyRequest(kid) {
-    return new Promise((resolve, reject) => {
-      request({uri: pubKeyUrl, json: true})
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          reject('pubkey request failed')
-        })
-    })
-  }
+    pubKeyRequest() {
+        return new Promise( ( resolve, reject ) => {
+            request( { "uri": pubKeyUrl, "json": true } )
+                .then( ( response ) => {
+                    resolve( response );
+                } )
+                .catch( ( error ) => {
+                    reject( error );
+                } );
+        } );
+    }
 
-  getUserInfo(accessToken) {
-    return new Promise((resolve, reject) => {
-      request({url: userInfoUrl, headers: { 'Authorization': ' Bearer ' + accessToken}, json: true})
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          reject(error);
-        })
-    })
-  }
+    getUserInfo( accessToken ) {
+        return new Promise( ( resolve, reject ) => {
+            request( { "url": userInfoUrl, "headers": { "Authorization": ` Bearer ${ accessToken}` }, "json": true } )
+                .then( ( response ) => {
+                    resolve( response );
+                } )
+                .catch( ( error ) => {
+                    reject( error );
+                } );
+        } );
+    }
 }
 
 module.exports = yconnect;
